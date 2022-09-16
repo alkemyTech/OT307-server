1b851e8cdb018fddcaa66a4a77378eb0cc55f2fd
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-09-16 13:17:06 -0500

    CHORE: migration discard to Testimonials

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_180305) do
+ActiveRecord::Schema.define(version: 2022_09_16_181123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2022_09_16_180305) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_testimonials_on_discarded_at"
   end
 
 end
--- spec/models/testimonial_spec.rb
+++ spec/models/testimonial_spec.rb
@@ -2,11 +2,16 @@
 #
 # Table name: testimonials
 #
-#  id         :bigint           not null, primary key
-#  content    :string
-#  name       :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id           :bigint           not null, primary key
+#  content      :string
+#  discarded_at :datetime
+#  name         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_testimonials_on_discarded_at  (discarded_at)
 #
 require 'rails_helper'
 
