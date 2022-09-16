d56fd03dbcb294d9c67eaf331cf26987a9281ccb
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-09-16 13:09:29 -0500

    CHORE: migration Testimonial

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: testimonials
+#
+#  id         :bigint           not null, primary key
+#  content    :string
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Testimonial < ApplicationRecord
     validates :name, presence: true
 end
