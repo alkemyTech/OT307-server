91f6c1688560979c10fc4073f007bd3887204ab5
Miguel Rivas <mrivaslezama@gmail.com>
2022-09-20 15:24:47 -0300

    again for robocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,11 @@
+# frozen_string_literal: true
 require 'json_web_token'
 
 module Api
   module V1
     class UsersController < ApplicationController
-      before_action :set_user, only: :destroy
-      before_action :set_user_for_login, only: :login
-      before_action :authenticate_request, only: %i[index me update destroy]
-      before_action :authorize_user, only: %i[index me destroy]
-
       
-    end 
-  end 
-end 
+      
+    end
+  end
+end
