f88159ee2df3f0924fbf4b208ae1d1c14bb3002f
Fernando Ciballero <fernandociballero@gmail.com>
2022-09-20 23:25:37 -0300

    feat/OT307-18-model-migration-controller-user

---
+++ app/models/user.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  discarded_at    :datetime
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  password_digest :string           not null
+#  photo           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  role_id         :bigint           not null
+#
+# Indexes
+#
+#  index_users_on_discarded_at  (discarded_at)
+#  index_users_on_email         (email) UNIQUE
+#  index_users_on_role_id       (role_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (role_id => roles.id)
+#
+class User < ApplicationRecord
+  include Discard::Model
+
+  has_secure_password
+  has_one_attached :image
+
+  belongs_to :role
+  has_many :comments, dependent: :destroy
+  has_many :contacts, dependent: :nullify
+
+  validates :first_name, :last_name, :email, :password, presence: true
+  validates :email, uniqueness: true
+end
