26423e82ef8ce00d5248bc58e35ef1f8f6b4bd36
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-09-21 17:09:24 -0500

    CHORE: create model and migration for Slides

---
+++ spec/factories/slides.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: slides
+#
+#  id              :bigint           not null, primary key
+#  image_url       :string
+#  order           :integer
+#  text            :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  organization_id :bigint           not null
+#
+# Indexes
+#
+#  index_slides_on_organization_id  (organization_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (organization_id => organizations.id)
+#
+FactoryBot.define do
+  factory :slide do
+    order { 1 }
+    text { "MyString" }
+    image_url { "MyString" }
+    organization { nil }
+  end
+end
