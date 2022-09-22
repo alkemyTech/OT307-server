4cf5a4f5afcb4f0e5d79a55105ebf3a2ab987acc
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-09-22 15:27:56 -0500

    CHORE: endpoint de autenticacion

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,24 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :set_user_for_login, only: :login
+      # before_action :authenticate_request, only: %i[index me update destroy]
+      def login
+        if @user.authenticate(params[:user][:password])
+          token = JsonWebToken.encode(user_id: @user.id)
+          render json: { token: token,
+                         exp: 15.minutes.after(Time.zone.now).strftime('%m-%d-%Y %H:%M'),
+                         username: @user.first_name }, status: :ok
+        else
+          render json: { error: 'unauthorized' }, status: :unauthorized
+        end
+      end
+
+      private
+
+      def set_user_for_login
+        @user = User.kept.find_by!(email: params[:user][:email])
+      end
     end
   end
 end
