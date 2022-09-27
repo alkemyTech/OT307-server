20ef24f36c34cf9f43cc211bcd1acc2acf9262c9
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-09-27 17:09:17 -0500

    CHORE: update users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :set_user_for_login, only: :login
-      # before_action :authenticate_request, only: %i[index me update destroy]
+      before_action :authenticate_request, only: %i[update]
       def login
         if @user.authenticate(params[:user][:password])
           token = JsonWebToken.encode(user_id: @user.id)
@@ -18,10 +18,22 @@ module Api
         end
       end
 
+      def update
+        if @current_user.update(user_params)
+          render json: UserSerializer.new(@user).serializable_hash, status: :ok
+        else
+          render json: @current_user.errors, status: :unprocessable_entity
+        end
+      end
+
       private
 
       def set_user_for_login
         @user = User.kept.find_by!(email: params[:user][:email])
+      end
+
+      def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :image)
       end
     end
   end
