810ab406c8150d80ad2513275f9b1864be16fe72
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2022-09-29 16:15:46 -0300

    OT307-34 ownership middleware

--- app/controllers/concerns/authorization.rb
+++ app/controllers/concerns/authorization.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Authorization
+  extend ActiveSupport::Concern
   def authorization
     return if administrator?
 
@@ -8,7 +9,18 @@ module Authorization
            status: :forbidden
   end
 
+  def owner?
+    params[:id].to_i == @current_user.id
+  end
+
   def administrator?
     @current_user.role.name == 'administrator'
+  end
+
+  def ownership?
+    return if administrator? || owner?
+
+    render json: { errors: 'Unauthorized access. Need administrator role to access' },
+           status: :forbidden
   end
 end
