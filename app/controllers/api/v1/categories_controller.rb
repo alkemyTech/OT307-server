0953901baae5bea28ae1be377426ea98b38dd7d5
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2022-09-30 15:00:17 -0300

    OT307-41-categories index

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -4,8 +4,14 @@ module Api
   module V1
     class CategoriesController < ApplicationController
       before_action :set_category, only: %i[show]
-      before_action :authenticate_request, only: %i[show]
-      before_action :authorization, only: %i[show]
+      before_action :authenticate_request, only: %i[index show]
+      before_action :authorization, only: %i[index show]
+
+      def index
+        @categories = Category.kept
+        render json: CategorySerializer.new(@categories,
+                                            { fields: { category: [:name] } }).serializable_hash
+      end
 
       def show
         render json: CategorySerializer.new(@category).serializable_hash, status: :ok
