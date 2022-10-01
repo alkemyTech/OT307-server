ba80730c207fd0696b73635e56be2ed2965a68b5
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-09-30 19:31:36 -0500

    CHORE: Eliminar Novedades

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -3,6 +3,28 @@
 module Api
   module V1
     class NewsController < ApplicationController
+      before_action :set_news, only: %i[destroy]
+      before_action :authenticate_request, only: %i[destroy]
+      before_action :authorization, only: %i[destroy]
+
+      def destroy
+        @news.discard
+        head :no_content
+      end
+
+      private
+
+      def set_news
+        @news = News.kept.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Could not find news with ID '#{params[:id]}'" }
+      end
+      
+      def news_params
+        params.require(:news).permit(:content, :name, :news_type, :category_id)
+      end
+
+
     end
   end
 end
