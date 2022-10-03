1472e17fb4f6689cf1ca111dcdedc8810a09f495
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2022-10-03 12:52:32 -0300

    OT307-38 Organization update data

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -3,13 +3,22 @@
 module Api
   module V1
     class OrganizationsController < ApplicationController
-      before_action :set_organization, only: %i[public]
-      before_action :authenticate_request, only: %i[public]
+      before_action :set_organization, only: %i[public update]
+      before_action :authenticate_request, only: %i[public update]
+      before_action :authorization, only: %i[update]
 
       def public
         render json: OrganizationSerializer.new(@organization, fields:
         { organization: %i[name image phone address] }).serializable_hash,
                status: :ok
+      end
+
+      def update
+        if @organization.update(org_params)
+          render json: OrganizationSerializer.new(@organization).serializable_hash, status: :ok
+        else
+          render json: @organization.errors, status: :unprocessable_entity
+        end
       end
 
       private
@@ -18,6 +27,11 @@ module Api
         @organization = Organization.kept.find(params[:id])
       rescue ActiveRecord::RecordNotFound
         render json: { error: "Couldn't find organization with ID '#{params[:id]}'" }
+      end
+
+      def org_params
+        params.require(:organization).permit(:about_us_text, :address, :email, :name, :phone,
+                                             :welcome_text)
       end
     end
   end
