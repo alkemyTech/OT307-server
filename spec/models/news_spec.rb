d1cfaa1cb7fda9770bc5556815a44222dbc6d6d1
rodrigo ladron de guevara <ro.ladrondeguevara@gmail.com>
2022-10-06 11:57:54 -0300

    feat/OT307-99-news-model-testing

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -24,5 +24,27 @@
 require 'rails_helper'
 
 RSpec.describe News, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:content) }
+  end
+
+  describe 'DB Columns' do
+    it { is_expected.to have_db_column(:id).of_type(:integer).with_options(null: false) }
+    it { is_expected.to have_db_column(:content).of_type(:text).with_options(null: false) }
+    it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
+    it { is_expected.to have_db_column(:discarded_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:category_id).of_type(:integer).with_options(null: false) }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to(:category) }
+  end
+
+  describe 'Indexes' do
+    it { is_expected.to have_db_index(:category_id) }
+    it { is_expected.to have_db_index(:discarded_at) }
+  end
 end
