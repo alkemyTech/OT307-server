aaaf5cd92bc61aefcc68e07088807a7eef45efbc
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-10-11 18:31:12 -0500

    CHORE: Actualizar Novedades

--- app/controllers/api/v1/news_controller.rb
+++ app/controllers/api/v1/news_controller.rb
@@ -3,6 +3,26 @@
 module Api
   module V1
     class NewsController < ApplicationController
+      before_action :set_news, only: %i[update]
+      before_action :authenticate_request, only: %i[update]
+      before_action :authorization, only: %i[update]
+
+      def update
+        @news.update(news_params)
+        render json: NewsSerializer.new(@news).serializable_hash, status: :ok
+      end
+
+      private
+
+      def set_news
+        @news = News.kept.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: "Could not find news with ID '#{params[:id]}'" }
+      end
+
+      def news_params
+        params.require(:news).permit(:content, :name, :category_id)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post "/auth/login", to: "users#login"
+      resources :news, only: [:update]
     end
   end
 end
